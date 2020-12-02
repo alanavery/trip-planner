68d8c33b36f4c8ce8001a3dbe56b22ce2d538166
delayedaa <delayedaa@gmail.com>
2020-12-02 11:00:19 -0600

    [seeders] Add seeders for cat. and subcat.

---
+++ models/subcategory.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class subcategory extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      models.subcategory.belongsTo(models.category);
+    }
+  };
+  subcategory.init({
+    name: DataTypes.STRING,
+    categoryId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'subcategory',
+  });
+  return subcategory;
+};
---
+++ seeders/20201202154813-categories.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('categories', [
+      {
+        name: 'Lodging',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Dining',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Activity',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Event',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Transportation',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('categories', null, {});
+  }
+};
