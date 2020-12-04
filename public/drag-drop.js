d5d95af53e24b79c85ee2479d9f778ac31ef95b3
delayedaa <delayedaa@gmail.com>
2020-12-04 16:40:44 -0600

    [drag-drop.js] Begin init gsap

---
+++ public/drag-drop.js
@@ -0,0 +1,15 @@
+// Register plugin with GSAP core
+gsap.registerPlugin(Draggable);
+
+let rowSize = 100;
+let container = document.querySelector('.container');
+let listItems = Array.from(document.querySelectorAll('.list-item'));
+let sortables = listItems.map(sortable);
+let total = sortables.length;
+
+gsap.to(container, {
+  duration: 2,
+  autoAlpha: 0
+});
+
+gsap.to(".box", { rotation: 27, x: 100, duration: 1 });
