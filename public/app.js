827115e9c93802bb9184b7b0ec9f2be8f358e48d
delayedaa <delayedaa@gmail.com>
2020-12-04 17:44:43 -0600

    [index.ejs] Fix gsap issue

--- public/app.js
+++ public/app.js
@@ -1,27 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-  let tripEdit = document.querySelector('.trip-edit');
-  let tripDelete = document.querySelector('.trip-delete');
-  let secTripEdit = document.querySelector('.sec-trip-edit');
-  let secTripDelete = document.querySelector('.sec-trip-delete');
-  let allAddSegmentButtons = document.querySelectorAll('.add-segment-button');
-  let divSegmentAll = document.querySelectorAll('.div-segment');
-  let divSegmentInfoSecondaryAll = document.querySelectorAll('.div-segment-info-secondary');
-  let segmentEditAll = document.querySelectorAll('.segment-edit');
-  let formSegmentEditAll = document.querySelectorAll('.form-segment-edit');
-  let divSegmentInfoAll = document.querySelectorAll('.div-segment-info');
-  let segmentEditCancelAll = document.querySelectorAll('.segment-edit-cancel');
-  let addSegmentSec = document.querySelector('.add-segment');
-  let addSegmentDate = document.getElementById('add-segment-date');
+console.log('app.js is working');
 
-  let visualReset = () => {
-    secTripDelete.hidden = true;
-    secTripEdit.hidden = true;
-    addSegmentSec.hidden = true;
-    formSegmentEditAll.forEach(form => form.hidden = true);
-    divSegmentInfoAll.forEach(div => div.hidden = false);
-    divSegmentInfoSecondaryAll.forEach(div => div.hidden = true);
-  };
+let tripEdit = document.querySelector('.trip-edit');
+let tripDelete = document.querySelector('.trip-delete');
+let secTripEdit = document.querySelector('.sec-trip-edit');
+let secTripDelete = document.querySelector('.sec-trip-delete');
+let allAddSegmentButtons = document.querySelectorAll('.add-segment-button');
+let divSegmentAll = document.querySelectorAll('.div-segment');
+let divSegmentInfoSecondaryAll = document.querySelectorAll('.div-segment-info-secondary');
+let segmentEditAll = document.querySelectorAll('.segment-edit');
+let formSegmentEditAll = document.querySelectorAll('.form-segment-edit');
+let divSegmentInfoAll = document.querySelectorAll('.div-segment-info');
+let segmentEditCancelAll = document.querySelectorAll('.segment-edit-cancel');
+let addSegmentSec = document.querySelector('.add-segment');
+let addSegmentDate = document.getElementById('add-segment-date');
 
+let visualReset = () => {
+  secTripDelete.hidden = true;
+  secTripEdit.hidden = true;
+  addSegmentSec.hidden = true;
+  formSegmentEditAll.forEach(form => form.hidden = true);
+  divSegmentInfoAll.forEach(div => div.hidden = false);
+  divSegmentInfoSecondaryAll.forEach(div => div.hidden = true);
+};
+
+if (tripEdit) {
   tripEdit.addEventListener('click', () => {
     if (secTripEdit.hidden) {
       visualReset();
@@ -31,7 +33,9 @@ document.addEventListener('DOMContentLoaded', () => {
       secTripEdit.hidden = true;
     }
   });
+}
 
+if (tripDelete) {
   tripDelete.addEventListener('click', () => {
     if (secTripDelete.hidden) {
       visualReset();
@@ -41,50 +45,43 @@ document.addEventListener('DOMContentLoaded', () => {
       secTripDelete.hidden = true;
     }
   });
+}
 
-  allAddSegmentButtons.forEach(button => {
-    button.addEventListener('click', event => {
+allAddSegmentButtons.forEach(button => {
+  button.addEventListener('click', event => {
+    visualReset();
+    addSegmentSec.hidden = false;
+    let selDay = event.target.parentNode.parentNode;
+    addSegmentDate.value = selDay.id.substring(4);
+  });
+});
+
+divSegmentAll.forEach(div => {
+  div.addEventListener('click', event => {
+    let selectedInfoSecondary = event.currentTarget.firstElementChild.lastElementChild;
+    let selectedFormSegmentEdit = event.currentTarget.lastElementChild;
+    if (selectedInfoSecondary.hidden) {
       visualReset();
-      addSegmentSec.hidden = false;
-      let selDay = event.target.parentNode.parentNode;
-      addSegmentDate.value = selDay.id.substring(4);
-      // let selectedForm = event.target.parentNode.nextElementSibling;
-      // if (selectedForm.hidden) {
-      //   selectedForm.hidden = false;
-      //   event.target.textContent = '-';
-      // } else {
-      //   visualReset();
-      // }
-    });
-  });
-
-  divSegmentAll.forEach(div => {
-    div.addEventListener('click', event => {
-      let selectedInfoSecondary = event.currentTarget.firstElementChild.lastElementChild;
-      let selectedFormSegmentEdit = event.currentTarget.lastElementChild;
-      if (selectedInfoSecondary.hidden) {
-        visualReset();
-        selectedInfoSecondary.hidden = false;
-      } else if (selectedFormSegmentEdit.hidden) {
-        visualReset();
-      }
-    });
-  });
-
-  segmentEditAll.forEach(button => {
-    button.addEventListener('click', event => {
-      let selectedSegmentInfo = event.target.parentNode.parentNode;
-      let selectedFormSegmentEdit = selectedSegmentInfo.nextElementSibling;
-      console.log(selectedFormSegmentEdit);
-      selectedSegmentInfo.hidden = true;
-      selectedFormSegmentEdit.hidden = false;
-    });
-  });
-
-  segmentEditCancelAll.forEach(button => {
-    button.addEventListener('click', event => {
-      event.preventDefault();
+      selectedInfoSecondary.hidden = false;
+    } else if (selectedFormSegmentEdit.hidden) {
       visualReset();
-    });
+    }
   });
-})
+});
+
+segmentEditAll.forEach(button => {
+  button.addEventListener('click', event => {
+    let selectedSegmentInfo = event.target.parentNode.parentNode;
+    let selectedFormSegmentEdit = selectedSegmentInfo.nextElementSibling;
+    console.log(selectedFormSegmentEdit);
+    selectedSegmentInfo.hidden = true;
+    selectedFormSegmentEdit.hidden = false;
+  });
+});
+
+segmentEditCancelAll.forEach(button => {
+  button.addEventListener('click', event => {
+    event.preventDefault();
+    visualReset();
+  });
+});
--- public/drag-drop.js
+++ public/drag-drop.js
@@ -1,15 +1,32 @@
-// Register plugin with GSAP core
-gsap.registerPlugin(Draggable);
+document.addEventListener('DOMContentLoaded', () => {
 
-let rowSize = 100;
-let container = document.querySelector('.container');
-let listItems = Array.from(document.querySelectorAll('.list-item'));
-let sortables = listItems.map(sortable);
-let total = sortables.length;
 
-gsap.to(container, {
-  duration: 2,
-  autoAlpha: 0
+
+
+  console.log('drag-drop.js is working');
+
+  // Register plugin with GSAP core
+  // gsap.registerPlugin(Draggable);
+
+  let rowSize = 100;
+  let container = document.querySelector('.container');
+  let listItems = Array.from(document.querySelectorAll('.list-item'));
+  // let sortables = listItems.map(sortable);
+  // let total = sortables.length;
+
+  gsap.to(container, {
+    duration: 2,
+    autoAlpha: 0
+  });
+
+  gsap.to(".box", { rotation: 27, x: 100, duration: 1 });
+
+
+  console.log('drag-drop.js is working here too');
+
+
+
+
+
 });
 
-gsap.to(".box", { rotation: 27, x: 100, duration: 1 });
